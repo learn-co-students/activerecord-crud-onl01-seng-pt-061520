9604cdd1cb4a788dc719f98e45b73dcd67f2f13e
Cameron Hudson <camnn3@yahoo.com>
2020-09-10 01:25:17 +0000

    Automatically backed up by Learn

--- db/migrate/20200909034614_create_movies.rb
+++ db/migrate/20200909034614_create_movies.rb
@@ -1,9 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    t.string :title
-    t.integer :release_date
-    t.string :director
-    t.string :lead
-    t.boolean :in_theaters
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
   end
 end
